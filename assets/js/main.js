95954ce831c947b6bdb908992ad43a3290a24f0e
Yen-ling Kuo <ylkuo@ylkuo-macbookair2.roam.corp.google.com>
2015-07-26 20:53:03 -0700

    add home and update url handling

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,6 @@
 
 (function($) {
   $.fn.goTo = function() {
-    console.log($(".navbar").height());
     $('html, body').animate({
       scrollTop: ($(this).offset().top - $(".navbar").height()) + 'px'
     }, 'fast');
@@ -20,10 +19,11 @@ function WebsiteUI() {
   var self = this;
   // Setup page navigation.
   self.setupHandlers();
-
-  $.get( "home.html", function(data) {
-    $( "#main" ).html(data);
+  // Setup History listener.
+  window.History.Adapter.bind(window, 'statechange', function() {
+    self.pageController(window.History.getState().hash, false);
   });
+  self.pageController(window.History.getState().hash, false);
 }
 
 /**
@@ -34,65 +34,70 @@ WebsiteUI.prototype.setupHandlers = function() {
 
   $(document).on('click', 'a.go-home', function(e) {
     e.preventDefault();
-    $.get("home.html", function(data) {
-      self.unsetActiveMenu();
-      $(".go-home").parent().addClass("active");
-      $("#header").show();
-      $("#main").html(data);
-      $(".navbar-collapse").collapse('hide');
-    });
-    $( window ).resize(function() {
-      $("#header").show();
-    });
+    self.go('/');
   });
 
   $(document).on('click', 'a.go-about', function(e) {
     e.preventDefault();
-    $.get("about.html", function(data) {
-      self.unsetActiveMenu();
-      $(".go-about").parent().addClass("active");
-      self.hideHeader();
-      $("#main").html(data);
-      $(".navbar-collapse").collapse('hide');
-    });
+    self.go('/?about');
   });
 
   $(document).on('click', 'a.go-project', function(e) {
     e.preventDefault();
-    $.get("project.html", function(data) {
-      self.unsetActiveMenu();
-      $(".go-project").parent().addClass("active");
-      self.hideHeader();
-      $("#main").html(data);
-      $(".navbar-collapse").collapse('hide');
-
-      self.setupProjectImageLightbox($('#cs-integration'));
-      self.setupProjectImageLightbox($('#language-explorer'));
-      self.setupProjectImageLightbox($('#analogyspace'));
-      self.setupProjectImageLightbox($('#virtualpets'));
-      self.setupProjectImageLightbox($('#movisee'));
-      self.setupProjectImageLightbox($('#cubicfilm'));
-      self.setupProjectImageLightbox($('#handpaintfilm'));
-      self.setupProjectImageLightbox($('#taiwanuxd'));
-      self.setupProjectImageLightbox($('#flora'));
-      self.setupProjectImageLightbox($('#panorama'));
-      self.setupProjectImageLightbox($('#sdio'));
-
-      $(document).on('click', 'a.go-movisee', function(e) { $('#movisee').goTo(); });
-      $(document).on('click', 'a.go-taiwanuxd', function(e) { $('#taiwanuxd').goTo(); });
-    });
+    self.go('/?project')
   });
 
   $(document).on('click', 'a.go-publication', function(e) {
     e.preventDefault();
-    $.get("publication.html", function(data) {
-      self.unsetActiveMenu();
-      $(".go-publication").parent().addClass("active");
-      self.hideHeader();
-      $("#main").html(data);
-      $(".navbar-collapse").collapse('hide');
-    });
+    self.go('/?publication')
   });
+};
+
+WebsiteUI.prototype.go = function(url) {
+  window.History.pushState(null, null, url);
+};
+
+WebsiteUI.prototype.goTop = function() {
+  $('html, body').animate({ scrollTop: 0 }, 'fast');
+};
+
+/**
+ * Set up handlers for url input.
+ */
+WebsiteUI.prototype.pageController = function(url) {
+  var self = this;
+
+  // Remove trailing slash from url so that we can parse it correctly.
+  if (url.substr(-1) === '/') {
+    url = url.substr(0, url.length - 1);
+  }
+  // Extract sub page from URL, if any.
+  var idx = url.indexOf('?');
+  var hash = (idx > 0) ? url.slice(idx + 1) : '';
+  var value = hash.split('=');
+
+  switch (value[0]) {
+    case 'about':
+      self.renderAbout();
+      self.goTop();
+      break;
+    case 'project':
+      if (value.length == 2) {
+        self.renderProject(value[1]);
+      } else {
+        self.renderProject('');
+        self.goTop();
+      }
+      break;
+    case 'publication':
+      self.renderPublication();
+      self.goTop();
+      break;
+    default:
+      self.renderHome();
+      self.goTop();
+      break;
+  }
 };
 
 /**
@@ -134,5 +139,70 @@ WebsiteUI.prototype.setupProjectImageLightbox = function(elem) {
     usePopupDefaultStyling: false,
     usePopupEasyClose: false,
     usePopupNav: true
+  });
+};
+
+WebsiteUI.prototype.renderHome = function() {
+  var self = this;
+  $.get("home.html", function(data) {
+    self.unsetActiveMenu();
+    $(".go-home").parent().addClass("active");
+    $("#header").show();
+    $("#main").html(data);
+    $(".navbar-collapse").collapse('hide');
+  });
+  $(window).resize(function() {
+    $("#header").show();
+  });
+};
+
+WebsiteUI.prototype.renderAbout = function() {
+  var self = this;
+  $.get("about.html", function(data) {
+    self.unsetActiveMenu();
+    $(".go-about").parent().addClass("active");
+    self.hideHeader();
+    $("#main").html(data);
+    $(".navbar-collapse").collapse('hide');
+  });
+};
+
+WebsiteUI.prototype.renderProject = function(bookmark) {
+  var self = this;
+  $.get("project.html", function(data) {
+    self.unsetActiveMenu();
+    $(".go-project").parent().addClass("active");
+    self.hideHeader();
+    $("#main").html(data);
+    $(".navbar-collapse").collapse('hide');
+
+    self.setupProjectImageLightbox($('#cs-integration'));
+    self.setupProjectImageLightbox($('#language-explorer'));
+    self.setupProjectImageLightbox($('#analogyspace'));
+    self.setupProjectImageLightbox($('#virtualpets'));
+    self.setupProjectImageLightbox($('#movisee'));
+    self.setupProjectImageLightbox($('#cubicfilm'));
+    self.setupProjectImageLightbox($('#handpaintfilm'));
+    self.setupProjectImageLightbox($('#taiwanuxd'));
+    self.setupProjectImageLightbox($('#flora'));
+    self.setupProjectImageLightbox($('#panorama'));
+    self.setupProjectImageLightbox($('#sdio'));
+
+    $(document).on('click', 'a.go-movisee', function(e) { $('#movisee').goTo(); });
+    $(document).on('click', 'a.go-taiwanuxd', function(e) { $('#taiwanuxd').goTo(); });
+
+    if (bookmark == 'movisee') { $('#movisee').goTo(); }
+    else if (bookmark == 'taiwanuxd') { $('#taiwanuxd').goTo(); }
+  });
+};
+
+WebsiteUI.prototype.renderPublication = function() {
+  var self = this;
+  $.get("publication.html", function(data) {
+    self.unsetActiveMenu();
+    $(".go-publication").parent().addClass("active");
+    self.hideHeader();
+    $("#main").html(data);
+    $(".navbar-collapse").collapse('hide');
   });
 };
